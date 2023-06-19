dc6a9675edd0db3466fad970a396b3433285bc25
Shivayogi Akki <sakki@mtu.edu>
2023-06-19 13:26:02 -0400

    mpc with go1 on  flat surface

--- mpc-v1/mjpc/tasks/tasks.cc
+++ mpc-v1/mjpc/tasks/tasks.cc
@@ -23,6 +23,7 @@
 #include "tasks/particle/particle.h"
 #include "tasks/quadrotor/quadrotor.h"
 #include "tasks/quadruped/quadruped.h"
+#include "tasks/quadruped-go1/quadruped-go1.h"
 #include "tasks/swimmer/swimmer.h"
 #include "tasks/walker/walker.h"
 
@@ -79,6 +80,11 @@ const TaskDefinition<const char*> kTasksArray[]{
         .name = "Quadruped Flat",
         .xml_path = "quadruped/task_flat.xml",
         .residual = &Quadruped::ResidualFloor,
+    },
+    {
+        .name = "Quadruped FlatGo1",
+        .xml_path = "quadruped-go1/task_flat1.xml",
+        .residual = &QuadrupedGo1::ResidualFloor,
     },
     {
         .name = "Hand",
